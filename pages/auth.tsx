78b79a84a61b8e5a574368a7a4e5823e1f148b31
sg-i <sgadov2vanya@yandex.ru>
2024-01-15 21:03:25 +0300

    auth fix

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -66,12 +66,12 @@ const Auth = () => {
   }, [email]);
 
   return (
-    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-      <div className="bg-black h-full w-full lg:bg-opacity-50">
+    <div className="relative h-full  w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black h-full  w-full lg:bg-opacity-50 ">
         <nav className="px-12 py-5">
           <img src="./images/logo.png" alt="Logo" className="h-12" />
         </nav>
-        <div className="flex justify-center">
+        <div className="flex justify-center ">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 rounded-md w-full">
             <h2 className="text-white mb-8 text-4xl font-semibold">
               {variant === 'login' ? 'Sign in' : 'Register'}
